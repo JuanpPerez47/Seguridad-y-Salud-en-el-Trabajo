0a31ef9bb45f1dc02208686fe3c42109c7c16a95
JuanpPerez47 <129331522+JuanpPerez47@users.noreply.github.com>
2025-04-10 06:41:49 -0500

    Update ProySST.py

--- ProySST.py
+++ ProySST.py
@@ -87,7 +87,7 @@ def draw_detections(image, boxes, class_ids, scores):
 
 # Interfaz Streamlit
 st.title("🦺 Detección de Seguridad con YOLOv8 (ONNX)")
-uploaded_file = st.file_uploader("📁 Sube una imagen", type=["jpg", "jpeg", "png"]) or st.camera_input("📷 O toma una foto") or (lambda url: Image.open(BytesIO(requests.get(url).content)) if url else None)( st.text_input("🌐 O pega el enlace de una imagen"))
+uploaded_file = st.file_uploader("📁 Sube una imagen", type=["jpg", "jpeg", "png"]) or st.camera_input("📷 O toma una foto")or st.text_input("O ingrese la URL de la imagen")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
