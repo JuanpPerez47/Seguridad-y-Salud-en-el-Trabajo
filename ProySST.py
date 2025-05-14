29105bccd2c766e9195714220af44dc6d3a07e3e
JuanpPerez47 <129331522+JuanpPerez47@users.noreply.github.com>
2025-05-13 20:42:16 -0500

    Update ProySST.py

--- ProySST.py
+++ ProySST.py
@@ -10,7 +10,7 @@ from gtts import gTTS  # Importación para convertir texto a voz
 
 # Cargar modelos
 modelo_personas = YOLO("yolov8n.pt")
-modelo_ppe = YOLO("best.pt")
+modelo_ppe = YOLO("yolov8s.pt")
 
 # Configuración de la página
 st.set_page_config(page_title="Sistema PPE Inteligente", layout="wide")
